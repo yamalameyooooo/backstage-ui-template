822527f489a4c7725be089a6681dfa826d6576cd
Shamsuddin Shamsuddin <sshamsud@redhat.com>
2024-02-22 16:32:43 +0530

    Added logic for whats new page in codegen

--- codegen.js
+++ codegen.js
@@ -91,48 +91,33 @@ const generatePages = (json) => {
     import { Page, PageSection, PageSectionVariants, TextContent, Title, Text } from '@patternfly/react-core';
     import * as React from 'react';
     
-    const LandingPage : React.FunctionComponent = () => {
+    const LandingPage: React.FunctionComponent = () => {
       //logic for whats new modal, if the user is opening app for first time or after a version bump,
-      //then the version from package.json wont match the client browser version and modal will pop up,
-      //and at the end new version will be saved to client browser so on next page opening modal wont show
+      //then the version from pakage.json wont match the clint browser version and modal will pop up,
+      //and at the end new version will be saved to client brwser so on next page opening modal wont show
       var appCurrentVersion = packageJson.version;
       var appLocalStorageVersion = localStorage.getItem('appVersion');
-    
-      if (appCurrentVersion != appLocalStorageVersion) {
-        //setting updated version to local storage
+      const updateAppVersion = () => {
         localStorage.setItem('appVersion', appCurrentVersion);
-    
-        //show Whats new
-        return (
-          <Page
-            additionalGroupedContent={
-              <PageSection variant={PageSectionVariants.light} isWidthLimited>
-                <TextContent>
-                  <Text component="h1">Landing Page</Text>
-                  <Text component="p">Some definition for Landing Page</Text>
-                </TextContent>
-                <WhatsNew />
-              </PageSection>
-            }
-          ></Page>
-        );
-      } else {
-        return (
-          <Page
-            additionalGroupedContent={
-              <PageSection variant={PageSectionVariants.light} isWidthLimited>
-                <TextContent>
-                  <Text component="h1">Landing Page</Text>
-                  <Text component="p">Some definition for Landing Page</Text>
-                </TextContent>
-              </PageSection>
-            }
-          ></Page>
-        );
       }
-    };
     
-    export { LandingPage };
+        return (
+          <Page
+            additionalGroupedContent={
+              <PageSection variant={PageSectionVariants.light} isWidthLimited>
+                <TextContent>
+                <Text component="h1">Landing Page</Text>
+                <Text component="p">Some definition for Landing Page</Text>
+                </TextContent>
+                {appCurrentVersion !== appLocalStorageVersion && <WhatsNew confirm={updateAppVersion}/>}
+                
+              </PageSection>
+            }
+          ></Page>
+        );
+    };
+  
+    export { LandingPage };  
     `;
   const genericPageTemplateContent = `
     import { Page, PageSection, PageSectionVariants, Text, TextContent } from '@patternfly/react-core';
